43c2b6f8ffcea17992820ac984c14cdbacc0d1f7
Ishaantheguy <ishaanksheth2@gmail.com>
2026-08-20 21:54:33 +0530

    fix: changed site in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ishaantheguy.in',
+	site: 'https://ishaantheguy.github.io',
 	integrations: [mdx(), sitemap(), icon()],
 	fonts: [
 		{
